9761841435afded2fba66787f50e8e512838e412
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-10-29 15:04:03 +0300

    Finish day1

---
+++ typescript/2019/day1/day1.ts
@@ -0,0 +1,24 @@
+import { modulesMassList } from "./inputs";
+
+/* Part One */
+
+const sumOfFuelRequirements = modulesMassList
+  .map((mass) => Math.floor(mass / 3) - 2)
+  .reduce((acc, curVal) => acc + curVal);
+
+console.log("Sum of fuel requirements: ", sumOfFuelRequirements);
+
+/* Part Two */
+
+const sumOfAllFuelRequirements = modulesMassList
+  .map((mass) => {
+    let fuelRequirements = 0;
+    while (mass > 0) {
+      mass = Math.floor(mass / 3) - 2;
+      if (mass > 0) fuelRequirements += mass;
+    }
+    return fuelRequirements;
+  })
+  .reduce((acc, curVal) => acc + curVal);
+
+console.log("Sum of all fuel requirements: ", sumOfAllFuelRequirements);
