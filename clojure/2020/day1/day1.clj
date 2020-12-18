8835fcdfa8cc8ebb86b7145e31416bc1a931f0ed
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2020-12-19 01:08:34 +0300

    Finish clojure day 1 part 2

--- clojure/2020/day1/day1.clj
+++ clojure/2020/day1/day1.clj
@@ -8,27 +8,40 @@
        (split-lines)
        (map read-string)))
 
-(def input-1 "2020/day1/input1.txt")
+(def input "2020/day1/input.txt")
 
-;; Day 1
 ;; Part 1
 
 (defn find-pair
-  [expense-report]
+  [expense-report sum]
   (reduce
-   (fn [numbers number]
-     (let [required-number (- 2020 number)]
-       (if (contains? numbers number)
-         (reduced  [number required-number])
-         (conj numbers required-number))))
+   (fn [nums cur-num]
+     (let [req-num (- sum cur-num)]
+       (if (contains? nums cur-num)
+         (reduced  [cur-num req-num])
+         (conj nums req-num))))
    #{}
    expense-report))
 
-(prn (apply * (find-pair (read-input input-1))))
+(prn (apply * (find-pair (read-input input) 2020)))
 
+;; Part 2
 
-;; Tests
+(defn drop-nth
+  [n coll]
+  (keep-indexed #(when (not= %1 n) %2) coll))
 
+(defn find-triplet
+  [expense-report sum]
+  (loop [n 0]
+    (let [item (nth expense-report n)
+          filtered-list (drop-nth n expense-report)
+          pair (find-pair filtered-list (- sum item))]
+      (if (not= 2 (count pair))
+        (recur (inc n))
+        (conj pair item)))))
+
+(prn (apply * (find-triplet (read-input input) 2020)))
 
 (def test-expense-report
   [1721
@@ -40,4 +53,6 @@
 
 (deftest fix-expense-report
   (testing "find two entries that sum up to 2020"
-    (is (= (apply + (find-pair test-expense-report)) 2020))))
+    (is (= (apply + (find-pair test-expense-report 2020)) 2020)))
+  (testing "find three entries that sum up to 2020"
+    (is (= (apply + (find-triplet test-expense-report 2020)) 2020))))
